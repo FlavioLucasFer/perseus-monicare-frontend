8b98e4178c7f05d33bd8c769b4acc3d0d0a7e7d2
Henrique Gonçalves de Oliveira <henriquegon.o@hotmail.com>
2021-09-29 10:21:57 -0300

    Login screen #16

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,14 +1,12 @@
-import 'materialize-css';
-import './index.css';
+import './Login.css';
 
 import { useState } from 'react';
-import { useHistory } from 'react-router';
 
-import Input from 'components/Input.jsx';
+import { useHistory } from 'react-router';
 
 function initialState() {
   return { 
-    user: '', 
+    username: '', 
     password: '' 
   };
 }
@@ -26,7 +24,8 @@ const UserLogin = () => {
         </div>
 
         <div className='row'>
-          <Input label='Usuário'
+          <input placeholder='username' />
+          {/* <Input label='Usuário'
             id='user'
             name='user'
             type='text'
@@ -37,11 +36,12 @@ const UserLogin = () => {
                 ...values,
                 user: e.target.value
               });
-            }} />
+            }} /> */}
         </div>
-        
+
         <div className='row'>
-          <Input label='Senha'
+          <input name='password' />
+          {/* <Input label='Senha'
             id='password'
             name='password'
             type='password'
@@ -52,12 +52,11 @@ const UserLogin = () => {
                 ...values,
                 password: e.target.value
               });
-            }} />
+            }} /> */}
         </div>
-        
+
         <div className='row'>
-          <button className='btn waves-effect waves-light' 
-            type='submit' 
+          <button type='submit' 
             name='action'
             onClick={() => {
               history.push('/');
